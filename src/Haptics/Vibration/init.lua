a37192a44e4a628061e2dcf4658820e3d835f857
riverc5 <vex@vexitorylabs.com>
2021-04-26 20:18:30 -0500

    add backlog comments

--- src/Haptics/Vibration/init.lua
+++ src/Haptics/Vibration/init.lua
@@ -2,6 +2,8 @@ local Types = require(script.Parent.Types)
 local Vibration = {}
 Vibration.__index = Vibration
 
+-- TODO HPTCS-5: Have Vibration accept multiple inputs/motors
+-- TODO HPTCS-7: Have Vibrations constructible by sounds
 function Vibration.new(input: Enum.UserInputType, motor: Enum.VibrationMotor, sequence: Types.vibrationSequence): Types.vibration
     assert(typeof(input) == "EnumItem", "input must be a valid UserInputType!")
     assert(typeof(motor) == "EnumItem", "motor must be a valid VibrationMotor!")
